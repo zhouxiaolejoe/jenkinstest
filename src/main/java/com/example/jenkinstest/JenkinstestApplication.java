50b00a2c7561f4879b5c0aed20f701e2acbfd875
zhouxiaolejoe <a864511019@163.com>
2021-07-14 10:47:07 +0800

    Initial commit

---
+++ src/main/java/com/example/jenkinstest/JenkinstestApplication.java
@@ -0,0 +1,20 @@
+package com.example.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinstestApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "success";
+    }
+}
